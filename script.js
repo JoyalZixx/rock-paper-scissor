aac613c360ae774d9e763b39dc43fb98f54cd3f8
JoyalZixx <joeljoymon2@gmail.com>
2024-11-11 21:06:07 +0300

    final

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
 let humanScore = 0;
 let computerScore = 0;
 let humanAnswer;
+let computerAnswer;
 
-let choice = Math.floor(Math.random() * 3) + 1;
+document.getElementById("rock").addEventListener('click', function() {
+    getHumanChoice("rock");
+});
+
+document.getElementById("paper").addEventListener('click', function() {
+    getHumanChoice("paper");
+});
+
+document.getElementById("scissors").addEventListener('click', function() {
+    getHumanChoice("scissors");
+});
+
+function getHumanChoice(choice) {
+    humanAnswer = choice;
+    getComputerChoice();
+    console.log(computerAnswer);        
+    playRound(humanAnswer, computerAnswer);
+}
 
 function getComputerChoice() {
-    let answer;
-    if (choice === 1) {
-        answer = "rock";
-    } else if (choice === 2) {
-        answer = "paper";
-    } else if (choice === 3) {
-        answer = "scissors";
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    computerAnswer = choices[randomIndex];
+    return computerAnswer;
+}
+function playRound(humanAnswer, computerAnswer) {
+    let result = "";
+    if (humanAnswer == "rock" && computerAnswer == "paper") {
+        result = "Paper beats Rock, Computer Wins!!";
+    } else if (humanAnswer == "rock" && computerAnswer == "scissors") {
+        result = 'You WIN!!';
+    } else if (humanAnswer == "paper" && computerAnswer == "scissors") {
+        result = 'Computer WINS!!';
+    } else if (humanAnswer == "paper" && computerAnswer == "rock") {
+        result = 'you WINS!!';
+    } else if (humanAnswer == "scissors" && computerAnswer == "rock") {
+        result = 'Computer WINS!!';
+    } else if (humanAnswer == "scissors" && computerAnswer == "paper") {
+        result = 'You WINS!!';
+    } else if (humanAnswer == "rock" && computerAnswer == "rock") {
+        result = 'Its a tie';
+    } else if (humanAnswer == "paper" && computerAnswer == "paper") {
+        result = 'Its a tie';
+    } else if (humanAnswer == "scissors" && computerAnswer == "scissors") {
+        result = 'Its a tie';
     }
-    return answer;
+    document.getElementById("answer").innerHTML = result;
 }
-console.log(getComputerChoice());
-
-function getHumanChoice() {
-    document.getElementById('rock').addEventListener('click', () => {
-        humanAnswer = 1;
-        document.getElementById('answer').innerText = "You chose rock";
-    });
-    document.getElementById('paper').addEventListener('click', () => {
-        humanAnswer = 2;
-        document.getElementById('answer').innerText = "You chose paper";
-    });
-    document.getElementById('scissors').addEventListener('click', () => {
-        humanAnswer = 3;
-        document.getElementById('answer').innerText = "You chose scissors";
-    });
-}
-    
